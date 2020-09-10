ae762989ceb43e3253d2c5ddaa1754ba6def7e5b
Georges TAVERNE <gtaverne@e2r4p2.42.fr>
2020-09-10 10:41:45 +0200

    Tout va bien jusqu au 6

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gtaverne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/09 16:33:25 by gtaverne          #+#    #+#             */
-/*   Updated: 2020/09/09 17:03:41 by gtaverne         ###   ########.fr       */
+/*   Created: 2020/09/10 09:42:41 by gtaverne          #+#    #+#             */
+/*   Updated: 2020/09/10 10:40:16 by gtaverne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,29 @@
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	char lft;
+	char rgh;
+
+	lft = 0;
+	while (lft < 100)
+	{
+		rgh = lft + 1;
+		lu = lft % 10 + '0';
+		lt = lft / 10 + '0';
+		while (rgh < 100)
+		{
+			putchar(lft / 10 + '0');
+			putchar(lft % 10 + '0');
+			write(1, " ", 1);
+			putchar(rgh / 10 + '0');
+			putchar(rgh % 10 + '0');
+			write(1, ", ", 2);
+			rgh++;
+		}
+		lft++;
+	}
 }
